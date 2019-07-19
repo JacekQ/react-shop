fbb2d34b38380634fd740c0c3a3c713957856ea5
jacek.kujaszewski <jacek.kujaszewski@intive.com>
2019-07-19 15:20:54 +0200

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+function App () {
+  return (
+    <div className='App'>
+      <Route exact path='/' component={HomePage} />
+    </div>
+  );
 }
 
 export default App;
